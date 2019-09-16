14ebfd57170825c8265cb602e6f31f5db3a9e5a0
bigbeard2017 <bigbeard@126.com>
2019-09-16 10:45:05 +0800

    add linux , mac system information, can read memory and disk

---
+++ HealthDetect/system/windows/systemInfo.go
@@ -0,0 +1,34 @@
+package windows
+
+// import (
+// 	"log"
+// 	"syscall"
+// 	"unsafe"
+// )
+
+// func GetDiskSpace(diskPath string) {
+// 	kernel32, err := syscall.LoadLibrary("Kernel32.dll")
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	defer syscall.FreeLibrary(kernel32)
+// 	GetDiskFreeSpaceEx, err := syscall.GetProcAddress(syscall.Handle(kernel32), "GetDiskFreeSpaceExW")
+
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+
+// 	lpFreeBytesAvailable := int64(0)
+// 	lpTotalNumberOfBytes := int64(0)
+// 	lpTotalNumberOfFreeBytes := int64(0)
+// 	r, a, b := syscall.Syscall6(uintptr(GetDiskFreeSpaceEx), 4,
+// 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(diskPath))),
+// 		uintptr(unsafe.Pointer(&lpFreeBytesAvailable)),
+// 		uintptr(unsafe.Pointer(&lpTotalNumberOfBytes)),
+// 		uintptr(unsafe.Pointer(&lpTotalNumberOfFreeBytes)), 0, 0)
+
+// 	log.Printf("Available  %dmb", lpFreeBytesAvailable/1024/1024.0)
+// 	log.Printf("Total      %dmb", lpTotalNumberOfBytes/1024/1024.0)
+// 	log.Printf("Free       %dmb", lpTotalNumberOfFreeBytes/1024/1024.0)
+
+// }
